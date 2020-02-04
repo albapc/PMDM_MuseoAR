f93afe067822f398ec986f6b5b5f8c8546e9337a
Alba <alba.perez.ce@gmail.com>
2020-02-04 16:42:55 +0100

    commit final

---
+++ Assets/Scripts/buttonHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+public class buttonHandler : MonoBehaviour
+{
+    bool sentence;
+    TrackableBehaviour tb;
+
+    public void ButtonClick()
+    {
+        if(sentence)
+        {
+            GameObject.Find("Button").GetComponentInChildren<Text>().text = "Play";
+
+            GameObject.Find("QuadSeta").GetComponent<UnityEngine.Video.VideoPlayer>().Pause();
+            GameObject.Find("QuadDoll").GetComponent<UnityEngine.Video.VideoPlayer>().Pause();
+            GameObject.Find("QuadCar").GetComponent<UnityEngine.Video.VideoPlayer>().Pause();
+            
+            sentence = false;
+        } else
+        {
+
+            GameObject.Find("Button").GetComponentInChildren<Text>().text = "Pause";
+
+            GameObject.Find("QuadSeta").GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+            GameObject.Find("QuadDoll").GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+            GameObject.Find("QuadCar").GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+
+            sentence = true;
+        }
+    }
+}
